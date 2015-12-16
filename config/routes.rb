22b3d26ee26f71dbfb91f229b8ef042b5f5f9f6e
projectman <obushmelev@gmail.com>
2015-12-16 13:15:41 +0300

    some minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   delete "/logout" => "sessions#destroy", as: "logout"
 
   resources :users, only: [:show, :new, :create, :destroy, :update]
+  get 'signup' => 'users#new'
   
 end
   
