57956034c43af739924f7f7ee8dad89309963f8e
projectman <obushmelev@gmail.com>
2015-12-26 10:24:40 +0300

    added some tests

---
+++ spec/requests/initial.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+
+describe "Initial tests: " do
+  
+  
+  
+  scenario "Visit index page without login not fail" do 
+    #When one visits index without login redirect to login page
+    visit (todo_lists_path)
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content('Login')
+  end
+
+
+  describe "GET /static_pages" do
+    # not pass to index without login	
+    it "test home page of application" do
+      visit '/todo_lists/index'
+      expect(page).not_to have_content('Listing Todo Lists')
+    end
+  end
+
+  # Prepare test for visiting whe user id is nil
+  # Preoare test redirect "new user" works for no ID user
+end
